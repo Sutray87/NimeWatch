841c7ec882c66d559ea3a059af3c14606d86c970
Sutray87 <muhamadmuslimfauzani@gmail.com>
2025-01-11 12:15:28 +0700

    adding responsive for logo in topbar

--- src/layout/topbar.jsx
+++ src/layout/topbar.jsx
@@ -3,7 +3,7 @@ import logoNM from '../../public/logo/logo-NimeWatch.png'
 export default function Topbar(){
     return(
         <div className='flex justify-between w-full px-2 items-center'>
-            <img src={logoNM} className='h-12'/>
+            <img src={logoNM} className='lg:h-12 sm:h-6'/>
             <div>
                 <input type="text" id="searchField" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-full w-96 focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
             </div>
